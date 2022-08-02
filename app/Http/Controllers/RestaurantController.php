f13680fce17b63facf997f618fb25e32a869576d
Ieva Baltriukaite <ieva.baltriukaite@gmail.com>
2022-08-02 13:58:58 +0300

    final commit

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -9,7 +9,7 @@ class RestaurantController extends Controller
 {
     public function __construct()
     {
-    //    $this->middleware('auth:api');
+     //   $this->middleware('auth:api');
     }
     /**
      * Display a listing of the resource.
